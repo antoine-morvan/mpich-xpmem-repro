871d93a34c634915e2dc77999348bfcf03f5e344
Antoine Morvan <antoine.morvan.phd@gmail.com>
2025-05-13 16:49:16 +0200

    Add HPE verison tryout ; add patch

--- repro.sh
+++ repro.sh
@@ -25,6 +25,11 @@ case ${ID_LIKE:-${ID}} in
 esac
 
 
+
+#################################################################################################################
+## HPC Vesrion
+#################################################################################################################
+
 XPMEM_SRC=$(readlink -f xpmem-2.6.3)
 XPMEM_BLD=$XPMEM_SRC
 XPMEM_PFX=$(readlink -f xpmem_prefix)
@@ -33,9 +38,14 @@ XPMEM_PFX=$(readlink -f xpmem_prefix)
 [ ! -f xpmem-2.6.3.tar.gz ] && curl -L -C - https://github.com/hjelmn/xpmem/archive/v2.6.3.tar.gz -o xpmem-2.6.3.tar.gz
 # rm -rf $XPMEM_SRC $XPMEM_BLD $XPMEM_PFX
 [ ! -d xpmem-2.6.3 ] && tar xf xpmem-2.6.3.tar.gz
+[ ! -f 63.patch ] && curl -L -C - https://patch-diff.githubusercontent.com/raw/hpc/xpmem/pull/63.patch -o 63.patch
+
+PATCH_1=$(readlink -f 63.patch)
+
 mkdir -p $XPMEM_BLD
 (
     cd $XPMEM_SRC
+    git apply $PATCH_1
     if [ ! -f $XPMEM_SRC/configure ]; then
         ./autogen.sh
     fi
@@ -52,3 +62,38 @@ mkdir -p $XPMEM_BLD
     fi
     make V=1 VERBOSE=1
 )
+exit 0
+
+#################################################################################################################
+## HPE Version
+#################################################################################################################
+
+XPMEM_PFX=$(readlink -f xpmem_prefix)
+rm -rf $XPMEM_PFX
+
+[ ! -d xpmem ] && git clone git@github.com:Cray-HPE/xpmem.git
+(
+    cd xpmem
+    (git clean -xdff && git checkout .)
+    if [ ! -f configure ]; then
+        ./autogen.sh
+    fi
+    if [ ! -f Makefile ]; then
+        ./configure --prefix=${XPMEM_PFX} \
+            --with-kerneldir=$KERNEL_SOURCE_DIR --with-kernelvers=${KERNEL_VERSION%.*}
+            # --with-module-prefix=/lib/modules/$(uname -r) \
+    fi
+    make 
+    make install
+)
+
+# (
+#     sudo rm -f /lib/modules/$(uname -r)/xpmem
+#     sudo ln -s \
+#         ${XPMEM_PFX}/lib/modules/5.14.0-503.40.1.el9_5/kernel/xpmem \
+#         /lib/modules/$(uname -r)/xpmem
+# )
+
+# modprobe xpmem
+
+exit 0
